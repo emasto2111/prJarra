d5216d8d7e0dd12b3884b3f535649ac26af837d3
emasto2111 <emanuelamariastoia@uma.es>
2021-03-12 10:32:18 +0100

    codigo programador 1

--- Jarra.java
+++ Jarra.java
@@ -11,16 +11,15 @@ con	capacidad	menor"	+
 this.capacidad=capacidadInicial;
 }
 //		metodos
-1
-2
-3
-public	void	metodoComunitario(){	
-//	Comentario	incluido	para	pruebas	
-System.out.println("Aqui	escribimos	todos");
-}
 public	int	capacidad(){
 return	this.capacidad;
 }
 public	int	cantidad(){
 return	this.cantidad;
+}
+2
+3
+public	void	metodoComunitario(){	
+//	Comentario	incluido	para	pruebas	
+System.out.println("Aqui	escribimos	todos");
 }
